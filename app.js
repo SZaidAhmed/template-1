ede95f36e2030973e241a6db193601b7aa035d33
Zaid Ahmed <ahmedsyedzaid@gmail.com>
2022-03-29 09:05:05 +0500

    2nd template

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require('path');
 
 
 const app = express();
-app.use(express.static(path.join(__dirname,'Hephaestus slider')));
+app.use(express.static(path.join(__dirname, 'Hephaestus slider')));
+app.use(express.static(path.join(__dirname, 'Destiny')));
 app.use(cors({ origin: true, credentials: true }));
 
 
@@ -15,6 +16,11 @@ app.use(express.json());
 app.get('/template', function (req, res) {
     console.log("first")
     res.sendFile(path.join(__dirname + '/Hephaestus slider/index.html'))
+});
+
+app.get('/template-1', function (req, res) {
+    console.log("second")
+    res.sendFile(path.join(__dirname + '/Destiny/index.html'))
 });
 
 module.exports = app;
